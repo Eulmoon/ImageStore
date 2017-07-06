4475257e165078ea7ec0720bc1f01a7b0cc86ddb
Eulmoon <Eulmoon@gmail.com>
2017-07-06 13:12:09 +0300

    Update site.js

--- ImageStore/wwwroot/js/site.js
+++ ImageStore/wwwroot/js/site.js
@@ -1,1 +1,35 @@
-﻿// Write your Javascript code.
+$(document).ready(
+    function () {
+        'use strict';
+        $('input:file').change(
+            function () {
+                $('input:submit').attr('disabled', true);
+                var f = this.files[0];
+                var Errorm = [];
+                var ext = this.value.match(/\.(.+)$/)[1];
+                if (f) {
+                    $('#ErrorText').hide();
+                }
+                if (f && (f.size > 20971520)) {
+                    Errorm.push('File above the limit (20MB)');
+                    $('#Maxsizeallow').show();
+                }
+                else {
+                    $('#Maxsizeallow').hide();
+                }
+                if (f && $.inArray(ext, ['gif', 'png', 'jpg', 'jpeg']) == -1) {
+                    Errorm.push('File type not allowed');
+                    $('#mimetypeallow').show();
+                }
+                else {
+                    $('#mimetypeallow').hide();
+                }
+                if (Errorm.length > 0) {
+                    alert(Errorm.join("\n"));                   
+                }
+                else {
+                    $('input:submit').attr('disabled', false);
+                }
+            }
+        );
+    });
